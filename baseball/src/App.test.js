8753f6dd0e19897d39f86611a8357c2a879413e7
nevets615 <surj316@yahoo.com>
2019-05-21 17:23:07 -0400

    initail pull request

---
+++ baseball/src/App.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import App from "./App";
+import * as rtl from "react-testing-library";
+import "jest-dom/extend-expect";
+import { render } from "react-testing-library";
+
+
+afterEach(rtl.cleanup);
+
+
+describe("<App />", () => {
+  it("renders without crashing", () => {
+  render(<App />)
+  });
+
+  it("check number of buttons", () => {
+    const {getButton} = render();
+    const input = getButton('2');
+
+    expectExport(input).toHaveAttribute("", "2")
+
+  });
+
+  it("check the state of balls and strikes on load", () => {
+
+
+  });
+
+  it("check the operation of the balls button", () => {
+
+
+  });
+
+  it("check the operation of the foul button", () => {
+
+
+  });
+
+  it("check the operation of the strike button", () => {
+
+
+  });
+
+  it("check the operation of the hit button", () => {
+
+
+  });
+});
